70b939ee5bdb623d9e35a24630c6d946ef0b5c83
GROMETTO N <nicolas.grometto@laposte.net>
2023-01-31 10:23:32 +0100

    file .env create

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://admin_NG:EdaVVr5c97NF8ET@cluster0.j9yjyof.mongodb.net/wordline';
+const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
  .then(() => console.log('Database connected'))
